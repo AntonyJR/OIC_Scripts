8cc881f50bdac14413c03119633c8cebff921347
AntonyJR <A.Reynolds@computer.org>
2024-01-05 16:14:50 -0700

    Create getCompartmentOCID.sh

---
+++ bin/getCompartmentOCID.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+  then
+    echo "Expected compartment name" >&2
+    echo "Usage : " $(basename "$0") "<Compartment_Name>" >&2
+    echo "Seaches a tenancy for the given compartment name and returns it for use in other commands"
+    echo "Compartment_Name is the name of the compartment to search for"
+    echo "Command docs https://docs.oracle.com/en-us/iaas/tools/oci-cli/3.37.1/oci_cli_docs/cmdref/iam/compartment/list.html"
+    exit 2
+fi
+oci iam compartment list --name $1 --compartment-id-in-subtree true --query 'data[0].id' | sed -e 's/^"//' -e 's/"$//'
